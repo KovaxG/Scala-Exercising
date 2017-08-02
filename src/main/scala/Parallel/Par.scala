386ebdbe817854f9d5b992f79d00ef296c5e264c
KovaxG <georgesmth202@gmail.com>
2017-08-02 13:09:03 +0300

    Finished Chapter 7

--- src/main/scala/Parallel/Par.scala
+++ src/main/scala/Parallel/Par.scala
@@ -36,5 +36,27 @@ object Par {
   // as requested by fork and extracting the resulting value
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
+  def asyncF[A,B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
+  //map2(parList, unit(()))((a, _) => a.sorted)
+
+  def map[A,B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a,_) => f(a))
+
+  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+    val fbs: List[Par[B]] = ps.map(asyncF(f))
+    sequence(fbs)
+  }
+
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ps.foldLeft(unit(Nil): Par[List[A]])((a, b) => map2(a, b)((x, y) => y :: x))
+
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] =
+    map(
+      parMap(as)(a => (f(a), a))
+    )( _.foldLeft(Nil: List[A])((a, b) => if (b._1) b._2 :: a else a))
+
+  def map3[A,B,C,D](a: Par[A], b: Par[B], c: Par[C])(f: (A, B, C) => D): Par[D] = {
+    def f2: (A, B) => C => D = (a, b) => c => f(a, b, c)
+    map2(map2(a, b)(f2), c)((g, e) => g(e))
+  }
 }
